9572c03e366b7adb0b0f7a6efd46357842db78d7
rawing97 <65003936+rawing97@users.noreply.github.com>
2020-12-20 18:21:36 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	include_once 'koneksi.php';
-	$sql = 'SELECT * FROM db_peminjam';
+	$sql = 'SELECT * FROM db_peminjam WHERE nim ORDER BY tanggal ASC';
 	$result = mysqli_query($conn,$sql);
 ?>
 
@@ -11,10 +11,10 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<img src="index.png">
+	<img src="gambar/index.png">
 	<h1>DATA PEMINJAM BUKU PERPUSTAKAAN</h1>
 	<div class="href">
-		<button><a href="tambah_data.php">TAMBAH DATA PEMINJAM</a></button>
+		<button style="font-size: 20px;"><a href="tambah_data.php">TAMBAH DATA PEMINJAM</a></button>
 	</div>
 	<div class="table">
 		<table border="1" ; cellpadding="10" ; cellspacing="0" ;>
@@ -25,6 +25,7 @@
 			<th>Nama</th>
 			<th>Nama Buku</th>
 			<th>Kelas</th>
+			<th>Aksi</th>
 		</tr>
 	<?php $no = 1; ?>
 	<?php while ($row = mysqli_fetch_assoc($result)):?>	
@@ -35,6 +36,11 @@
 			<td><?php echo $row['nama'];?> </td>
 			<td><?php echo $row['nama_buku'];?> </td>
 			<td><?php echo $row['kelas'];?> </td>
+			<td>
+				<a href="update.php?nim=<?php echo $row['nim'];?>" onclick="return confirm('Lanjut mengedit data <?php echo $row['nama'];?>')">Update</a> / 
+				
+				<a href="delete.php?nim=<?php echo $row['nim'];?>" onclick="return confirm('Apakah anda ingin menghapus data atas Nama : <?php echo $row['nama'];?> dengan NIM = <?php echo $row['nim'];?>...? ');">Delete</a>
+			</td>
 		</tr>
 		<?php $no++ ?>
 		<?php endwhile; ?>
